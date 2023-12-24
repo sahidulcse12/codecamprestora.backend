d3c3864e6d1bc9ec89f67bb841e960c7b239af82
muhit <sahid.299350@gmail.com>
2023-12-25 00:45:54 +0600

    generic repository added

---
+++ CodeCampRestora.Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using CodeCampRestora.Application.Common.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampRestora.Infrastructure.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+
+        public Task<T?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
