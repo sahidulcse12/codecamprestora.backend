e4a6c177b46ac00533af68328b3bd5a8bd997173
Shafayet Maruf <shafayetmaruf@gmail.com>
2023-12-24 18:34:04 +0600

    added separate configuration file for dependency injection in specific projects

---
+++ CodeCampRestora.Application/ServicesConfiguration.cs
@@ -0,0 +1,10 @@
+namespace CodeCampRestora.Application;
+
+public static class ServicesConfiguration
+{
+    public static IServiceProvider AddApplicationServices(this IServiceProvider services)
+    {
+
+        return services;
+    }
+}
---
+++ CodeCampRestora.Infrastructure/ServicesConfguration.cs
@@ -0,0 +1,10 @@
+namespace CodeCampRestora.Infrastructure;
+
+public static class ServicesConfiguration
+{
+    public static IServiceProvider AddInfrastructureServices(this IServiceProvider services)
+    {
+
+        return services;
+    }
+}
