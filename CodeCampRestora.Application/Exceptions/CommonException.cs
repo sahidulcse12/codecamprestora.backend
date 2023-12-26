0f197b1b7b3c58773c781b1cb8a2573517988a81
rehanmosabbir <rehanmosabbir@gmail.com>
2023-12-26 14:59:16 +0600

    conf: add CommonException class

---
+++ CodeCampRestora.Application/Exceptions/CommonException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampRestora.Application.Exceptions
+{
+    public class CommonException : Exception
+    {
+        public CommonException() : base() { }
+
+        public CommonException(string message) : base(message) { }
+
+        public CommonException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
