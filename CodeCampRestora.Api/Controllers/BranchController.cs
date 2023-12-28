41a4b8c539062892186aab802c83b4a985ebaaed
rafi <irafiulislam50@gmail.com>
2023-12-28 15:38:49 +0600

    feat: change BranchController

---
+++ CodeCampRestora.Api/Controllers/BranchController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCampRestora.Api.Controllers
+{
+    [Route("api/branch")]
+    [ApiController]
+    public class BranchController : ControllerBase
+    {
+    }
+}
