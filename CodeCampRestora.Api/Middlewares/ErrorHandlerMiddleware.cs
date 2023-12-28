39d7e12947137dbc01140fc4c7ed96a3a11fe58a
rehanmosabbir <rehanmosabbir@gmail.com>
2023-12-28 12:59:54 +0600

    conf: Inject ILogger to the ErrorHandleMiddleware

--- CodeCampRestora.Api/Middlewares/ErrorHandlerMiddleware.cs
+++ CodeCampRestora.Api/Middlewares/ErrorHandlerMiddleware.cs
@@ -13,10 +13,12 @@ namespace WebApi.Middlewares
     public class ErrorHandlerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
 
-        public ErrorHandlerMiddleware(RequestDelegate next)
+        public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext context)
@@ -29,7 +31,7 @@ namespace WebApi.Middlewares
             {
                 var response = context.Response;
                 response.ContentType = "application/json";
-                var responseModel = new Response<string>() { Succeeded = false, Message = error?.Message };
+                var responseModel = new Response<string>() { Succeeded = false, Message = error.Message };
                 
                 switch (error)
                 {
@@ -40,6 +42,7 @@ namespace WebApi.Middlewares
                         response.StatusCode = (int)HttpStatusCode.NotFound;
                         break;
                     default:
+                        _logger.LogError(error, error?.Message);
                         response.StatusCode = (int)HttpStatusCode.InternalServerError;
                         break;
                 }
