a7c96d9ee88a5dd9807b0d5f04ac303e72ed4526
muhit <sahid.299350@gmail.com>
2023-12-29 00:10:04 +0600

    fix : remove some unnecessay lines

--- CodeCampRestora.Infrastructure/Data/Repositories/GenericRepository.cs
+++ CodeCampRestora.Infrastructure/Data/Repositories/GenericRepository.cs
@@ -2,48 +2,47 @@
 using CodeCampRestora.Infrastructure.Data.DbContexts;
 using Microsoft.EntityFrameworkCore;
 
-namespace CodeCampRestora.Infrastructure.Data.Repositories
+namespace CodeCampRestora.Infrastructure.Data.Repositories;
+
+public class GenericRepository<T> : IRepository<T> where T : class
 {
-    public class GenericRepository<T> : IRepository<T> where T : class
+    private readonly AppDbContext _applicationDbContext;
+
+    public GenericRepository(AppDbContext applicationDbContext)
     {
-        private readonly AppDbContext _applicationDbContext;
+        _applicationDbContext = applicationDbContext;
+    }
 
-        public GenericRepository(AppDbContext applicationDbContext)
-        {
-            _applicationDbContext = applicationDbContext;
-        }
+    public async Task<IList<T>> GetAllAsync()
+    {
+        return await _applicationDbContext.Set<T>().ToListAsync();
+    }
 
-        public async Task<IList<T>> GetAllAsync()
-        {
-            return await _applicationDbContext.Set<T>().ToListAsync();
-        }
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _applicationDbContext.Set<T>().FindAsync(id);
+    }
 
-        public async Task<T?> GetByIdAsync(int id)
-        {
-            return await _applicationDbContext.Set<T>().FindAsync(id);
-        }
+    public async Task AddAsync(T entity)
+    {
+        await _applicationDbContext.Set<T>().AddAsync(entity);
+        await _applicationDbContext.SaveChangesAsync();
+    }
 
-        public async Task AddAsync(T entity)
+    public async Task DeleteAsync(int id)
+    {
+        var result = await GetByIdAsync(id);
+        if (result is null)
         {
-            await _applicationDbContext.Set<T>().AddAsync(entity);
-            await _applicationDbContext.SaveChangesAsync();
+            return;
         }
+        _applicationDbContext.Set<T>().Remove(result);
+        await _applicationDbContext.SaveChangesAsync();
+    }
 
-        public async Task DeleteAsync(int id)
-        {
-            var result = await GetByIdAsync(id);
-            if (result is null)
-            {
-                return;
-            }
-            _applicationDbContext.Set<T>().Remove(result);
-            await _applicationDbContext.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(int id, T entity)
-        {
-            _applicationDbContext.Set<T>().Update(entity);
-            await _applicationDbContext.SaveChangesAsync();
-        }
+    public async Task UpdateAsync(int id, T entity)
+    {
+        _applicationDbContext.Set<T>().Update(entity);
+        await _applicationDbContext.SaveChangesAsync();
     }
 }
