82072705550cb9e85b138737e552e81e3f13109c
muhit <sahid.299350@gmail.com>
2023-12-29 22:12:36 +0600

    feat : unit of work implemented

---
+++ CodeCampRestora.Infrastructure/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using CodeCampRestora.Application.Common.Interfaces.Repositories;
+using CodeCampRestora.Infrastructure.Data.DbContexts;
+
+namespace CodeCampRestora.Infrastructure.Data.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _appplicationDbContext;
+        public UnitOfWork(AppDbContext applicationDbContext) 
+        { 
+            _appplicationDbContext = applicationDbContext;
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _appplicationDbContext.SaveChangesAsync();
+        }
+    }
+}
