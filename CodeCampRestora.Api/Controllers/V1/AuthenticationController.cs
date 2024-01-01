85f3bda0401a28303f154244e6d4ed4eb0e5b414
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-01 11:53:21 +0600

    feat: prepared authentication controller

---
+++ CodeCampRestora.Api/Controllers/V1/AuthenticationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCampRestora.Api.Controllers.V1;
+
+public class AuthenticationController : ApiBaseController
+{
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
