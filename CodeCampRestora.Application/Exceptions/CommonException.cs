8ec986deb668bfb937521b558320e118c85ec1e6
rehanmosabbir <rehanmosabbir@gmail.com>
2024-01-01 11:13:49 +0600

    conf: Add AuthorizationException class

--- CodeCampRestora.Application/Exceptions/CommonException.cs
+++ CodeCampRestora.Application/Exceptions/CommonException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace CodeCampRestora.Application.Exceptions
         public CommonException(string message) : base(message) { }
 
         public CommonException(string message, Exception innerException) : base(message, innerException) { }
+        public virtual HttpStatusCode StatusCode { get; }
     }
 }
