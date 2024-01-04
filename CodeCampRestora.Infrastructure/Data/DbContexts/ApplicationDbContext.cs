59b90c7dab45c7bf86a601809fc769821901e88c
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-04 17:44:43 +0600

    feat: add MenuItem db set

--- CodeCampRestora.Infrastructure/Data/DbContexts/ApplicationDbContext.cs
+++ CodeCampRestora.Infrastructure/Data/DbContexts/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CodeCampRestora.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodeCampRestora.Infrastructure.Data.DbContexts
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
+        public DbSet<MenuItem> menuItems => Set<MenuItem>();
     }
 }
