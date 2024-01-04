3f4c1614f45d7cb85516694674fef3dc44a5adc7
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-04 22:45:55 +0600

    fix: add required usings

--- CodeCampRestora.Domain/Entities/MenuItem.cs
+++ CodeCampRestora.Domain/Entities/MenuItem.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using CodeCampRestora.Infrastructure.Entities;
 
 namespace CodeCampRestora.Domain.Entities
 {
