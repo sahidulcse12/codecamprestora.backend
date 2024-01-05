776d96792f7888823e6dc50957fe4ff0046fc129
Jafor Iqbal <jaforiqbal5593@gmail.com>
2024-01-05 11:38:22 +0600

    feat: restaurant creation api endpoint added

---
+++ CodeCampRestora.Api/Controllers/ReataurantController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CodeCampRestora.Infrastructure.Data.DbContexts;
+using CodeCampRestora.Infrastructure.Entities;
+using CodeCampRestora.Application.Dtos;
+using CodeCampRestora.Application.Models;
+using IResult = CodeCampRestora.Application.Models.IResult;
+
+namespace CodeCampRestora.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReataurantController : ApiBaseController
+{
+
+    private readonly ApplicationDbContext _dbcontext;
+
+    public ReataurantController(ApplicationDbContext dbContext)
+    {
+        _dbcontext = dbContext;
+    }
+
+    [HttpPost]
+    public async Task<IResult> Create([FromBody] RestaurantDto restaurant)
+    {
+        var RestaurantModel = new Restaurant
+        {
+            Name = restaurant.Name,
+            ImageId = restaurant.ImageId
+        };
+
+        _dbcontext.Restaurants.Add(RestaurantModel);
+        _dbcontext.SaveChanges();
+
+        return Result.Success();
+    }
+}
+
---
+++ CodeCampRestora.Application/Dtos/RestaurantDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeCampRestora.Application.Dtos;
+
+public class RestaurantDto
+{
+    public string Name { get; set; } = default!;
+    public Guid ImageId { get; set; } = default!;
+}
