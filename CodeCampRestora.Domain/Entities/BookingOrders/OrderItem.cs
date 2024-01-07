bbe80b265fefe113ce835e1eb5105887e2c19f0a
rehanmosabbir <rehanmosabbir@gmail.com>
2024-01-07 13:44:18 +0600

    feat: added OrderItem entity

---
+++ CodeCampRestora.Domain/Entities/BookingOrders/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using CodeCampRestora.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampRestora.Domain.Entities.BookingOrders
+{
+    public class OrderItem : AuditableEntity<Guid>
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Guid BookingOrderId { get; set; }
+        public Guid MenuItemId { get; set; }
+    }
+}
