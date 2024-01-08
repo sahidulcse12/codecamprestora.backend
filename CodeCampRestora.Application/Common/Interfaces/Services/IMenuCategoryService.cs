071864b243776a427b9c9ebfa477165fce989d2b
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-08 15:57:12 +0600

    feat: define IMenuCategoryService interface

---
+++ CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
@@ -0,0 +1,8 @@
+using CodeCampRestora.Application.Models;
+using CodeCampRestora.Domain.Entities;
+
+namespace CodeCampRestora.Application.Common.Interfaces.Services;
+public interface IMenuCategoryService
+{
+    Task<IResult<Guid>> CreateCategoryAsynce(MenuCategory menuCategory);
+}
