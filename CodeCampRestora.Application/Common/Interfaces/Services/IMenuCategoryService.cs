1f95ec6711e6d4f3318dc3a7cdb7caf3a9be8de2
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-08 17:31:02 +0600

    fix: fix a typo

--- CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
+++ CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
@@ -4,6 +4,6 @@ using CodeCampRestora.Domain.Entities;
 namespace CodeCampRestora.Application.Common.Interfaces.Services;
 public interface IMenuCategoryService
 {
-    Task<IResult<Guid>> CreateCategoryAsynce(MenuCategory menuCategory);
+    Task<IResult<Guid>> CreateCategoryAsync(MenuCategory menuCategory);
     Task<IResult> DeleteCategoryAsync(Guid Id);
 }
