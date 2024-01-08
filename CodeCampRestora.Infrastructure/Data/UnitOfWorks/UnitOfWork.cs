ed248471e2e8b4ec2acd58950c17076599d0cafb
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-08 16:06:28 +0600

    feat: extend UnitOfWork with MenuCategory

--- CodeCampRestora.Infrastructure/Data/UnitOfWorks/UnitOfWork.cs
+++ CodeCampRestora.Infrastructure/Data/UnitOfWorks/UnitOfWork.cs
@@ -11,16 +11,20 @@ public class UnitOfWork : IUnitOfWork
     public IImageRepository Images { get; }
     public IMenuItemRepository MenuItem { get; }
 
+    public IMenuCategoryRepository MenuCategory { get; }
+
     private readonly IApplicationDbContext _appplicationDbContext;
 
     public UnitOfWork(
         IImageRepository images,
         IMenuItemRepository menuItem,
+        IMenuCategoryRepository menuCategory,
         IApplicationDbContext applicationDbContext)
     {
         _appplicationDbContext = applicationDbContext;
         Images = images;
         MenuItem = menuItem;
+        MenuCategory = menuCategory;
     }
 
     public async Task SaveChangesAsync()
