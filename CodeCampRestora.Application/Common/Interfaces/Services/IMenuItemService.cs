5ca30b1f5986f9cab86431026e4e52dd3a9b4ed2
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-08 13:47:03 +0600

    feat: implement IMenuService

---
+++ CodeCampRestora.Application/Common/Interfaces/Services/IMenuItemService.cs
@@ -0,0 +1,10 @@
+using CodeCampRestora.Application.Models;
+using CodeCampRestora.Domain.Entities;
+
+namespace CodeCampRestora.Application.Common.Interfaces.Services
+{
+    public interface IMenuItemService
+    {
+        Task<IResult> CreateItemAsync(MenuItem menuItem);
+    }
+}
