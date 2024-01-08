9597e6433dca2b43f73333cd493d3fccb5ff15b7
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-08 17:13:05 +0600

    feat: add delete controller

--- CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
+++ CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
@@ -1,3 +1,4 @@
+using CodeCampRestora.Application.Features.MenuCategories.Commands.DeleteMenuCategory;
 using CodeCampRestora.Application.Features.MenuItems.Commands.CreateMenuCategory;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -11,6 +12,14 @@ public class MenuCategoryController : ApiBaseController
     public async Task<IResult> Post([FromBody] CreateMenuCategoryCommand command)
     {
         var result = Sender.Send(command);
+        return (IResult)result;
+    }
+
+    [HttpDelete("{id:Guid}")]
+    [SwaggerOperation(summary: "Delete a menu category")]
+    public async Task<IResult> Delete(Guid Id)
+    {
+        var result = Sender.Send(new DeleteMenuCategoryCommand(Id));
         return (IResult)result;
     }
 }
