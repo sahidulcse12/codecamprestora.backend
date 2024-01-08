3eab34f9e34b0823f6eefb5d426c5d802ba40e0c
rehanmosabbir <rehanmosabbir@gmail.com>
2024-01-08 15:01:35 +0600

    feat: added BookingOrdersController

---
+++ CodeCampRestora.Api/Controllers/V1/BookingOrdersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCampRestora.Api.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingOrdersController : ApiBaseController
+    {
+    }
+}
