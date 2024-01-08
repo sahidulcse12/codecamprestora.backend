17566f8c492fc18da20a2a6889caf0f9f8909de4
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-08 17:34:22 +0600

    remove: remove DeleteMenuCategory

--- CodeCampRestora.Application/Features/MenuCategories/Commands/DeleteMenuCategory/DeleteMenuCategory.cs
+++
@@ -1,5 +0,0 @@
-using CodeCampRestora.Application.Common.Interfaces.MediatRs;
-using CodeCampRestora.Application.Models;
-
-namespace CodeCampRestora.Application.Features.MenuCategories.Commands.DeleteMenuCategory;
-public record DeleteMenuCategory(Guid Id) : ICommand<IResult>;
