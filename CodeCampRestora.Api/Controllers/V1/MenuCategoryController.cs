cc2299ec7f6cea13d6f0add5b8ccef5974588088
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-09 11:31:29 +0600

    feat: add get by id controller

--- CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
+++ CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
@@ -1,5 +1,8 @@
+using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Application.Features.MenuCategories.Commands.DeleteMenuCategory;
+using CodeCampRestora.Application.Features.MenuCategories.Queries;
 using CodeCampRestora.Application.Features.MenuItems.Commands.CreateMenuCategory;
+using CodeCampRestora.Application.Models;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -9,17 +12,37 @@ public class MenuCategoryController : ApiBaseController
 {
     [HttpPost]
     [SwaggerOperation(summary: "Create a menu category")]
-    public async Task<IResult> Post([FromBody] CreateMenuCategoryCommand command)
+    public async Task<Application.Models.IResult> Post([FromBody] CreateMenuCategoryCommand command)
     {
         var result = Sender.Send(command);
-        return (IResult)result;
+        return (Application.Models.IResult)result;
+    }
+
+    [HttpGet("{id:Guid}")]
+    [SwaggerOperation(
+        Summary = "Get a menu Category",
+        Description = @"Sample Request:
+        Get: api/v1/MenuCategory/3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c"
+    )]
+    public async Task<IResult<MenuCategoryDto>> Get(
+        [FromRoute, SwaggerParameter(Description = "Get menu category by id", Required = true)]
+        Guid id)
+    {
+        var result = await Sender.Send(new GetMenuCategoryByIdQuery(id));
+        return result;
     }
 
     [HttpDelete("{id:Guid}")]
-    [SwaggerOperation(summary: "Delete a menu category")]
-    public async Task<IResult> Delete(Guid Id)
+    [SwaggerOperation(
+        Summary = "Delete a menu category",
+        Description = @"Sample Request:
+        Get: api/v1/MenuCategory/3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c"
+    )]
+    public async Task<Application.Models.IResult> Delete(
+        [FromRoute, SwaggerParameter(Description = "Delete by id", Required = true)]
+        Guid id)
     {
-        var result = Sender.Send(new DeleteMenuCategoryCommand(Id));
-        return (IResult)result;
+        var result = await Sender.Send(new DeleteMenuCategoryCommand(id));
+        return result;
     }
 }
