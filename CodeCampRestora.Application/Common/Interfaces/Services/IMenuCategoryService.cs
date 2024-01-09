01584bd39a66e72e4637d3d5bdeffa75a175d873
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-09 11:32:59 +0600

    feat: define GetMenuCategoryByIdAsync method

--- CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
+++ CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
@@ -1,3 +1,4 @@
+using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Application.Models;
 using CodeCampRestora.Domain.Entities;
 
@@ -5,5 +6,6 @@ namespace CodeCampRestora.Application.Common.Interfaces.Services;
 public interface IMenuCategoryService
 {
     Task<IResult<Guid>> CreateCategoryAsync(MenuCategory menuCategory);
+    Task<IResult<MenuCategoryDto>> GetMenuCategoryByIdAsync(Guid Id);
     Task<IResult> DeleteCategoryAsync(Guid Id);
 }
