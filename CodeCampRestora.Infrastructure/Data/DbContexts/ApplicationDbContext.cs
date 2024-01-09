8272c7cb40a4d3205c223ee1ee0eb784a9b9310b
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-09 12:58:05 +0600

    adding migration and DbContext

--- CodeCampRestora.Infrastructure/Data/DbContexts/ApplicationDbContext.cs
+++ CodeCampRestora.Infrastructure/Data/DbContexts/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using CodeCampRestora.Domain.Entities;
 using CodeCampRestora.Application.Attributes;
 using CodeCampRestora.Infrastructure.Entities;
 using CodeCampRestora.Application.Common.Interfaces.DbContexts;
+using CodeCampRestora.Domain.Entities.Reviews;
 
 namespace CodeCampRestora.Infrastructure.Data.DbContexts;
 
@@ -13,4 +14,5 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
 
     public DbSet<Image> Images => Set<Image>();
     public DbSet<Restaurant> Restaurants => Set<Restaurant>();
+    public DbSet<Reviews> Reviews => Set<Reviews>();
 }
---
+++ CodeCampRestora.Infrastructure/Data/Migrations/20240109064606_addedReviewTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeCampRestora.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedReviewTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedBy = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "text", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
