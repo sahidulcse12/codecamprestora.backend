faa48777a4225c47eae52381ab4636ed451737dd
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-09 11:44:27 +0600

    adding a class

---
+++ CodeCampRestora.Domain/Entities/Reviews/reviews.cs
@@ -0,0 +1,17 @@
+﻿using CodeCampRestora.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampRestora.Domain.Entities.Reviews
+{
+    public class Reviews : AuditableEntity<Guid>
+    {
+        public string Description { set; get; }
+        public int Rating { set; get; }
+        
+
+    }
+}
