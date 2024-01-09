5a3e77c601f0171806799a960693ba0ce8a7da0d
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-09 13:55:25 +0600

    adding reviewclass

---
+++ CodeCampRestora.Application/DTOs/ReviewDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCampRestora.Application.DTOs
+{
+    public class ReviewDTO
+    {
+        public string Description { set; get; }
+        public int Rating { set; get; }
+        public Guid OrderId { set; get; }
+    }
+}
