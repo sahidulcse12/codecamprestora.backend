19a686b1b19d20b9f13f693a3154cbb878cc3f35
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-09 11:37:01 +0600

    feat: add GetMenuItemByIdAsync service

--- CodeCampRestora.Application/Services/MenuItemService.cs
+++ CodeCampRestora.Application/Services/MenuItemService.cs
@@ -1,36 +1,49 @@
 using CodeCampRestora.Application.Attributes;
 using CodeCampRestora.Application.Common.Interfaces.Repositories;
 using CodeCampRestora.Application.Common.Interfaces.Services;
+using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Application.Models;
 using CodeCampRestora.Domain.Entities;
+using Mapster;
 using Microsoft.AspNetCore.Http;
 
-namespace CodeCampRestora.Application.Services
+namespace CodeCampRestora.Application.Services;
+[ScopedLifetime]
+public class MenuItemService : IMenuItemService
 {
-    [ScopedLifetime]
-    public class MenuItemService : IMenuItemService
+    private readonly IUnitOfWork _unitOfWork;
+    public MenuItemService(IUnitOfWork unitOfWork)
     {
-        private readonly IUnitOfWork _unitOfWork;
-        public MenuItemService(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-        }
-        public async Task<Models.IResult> CreateItemAsync(MenuItem menuItem)
-        {
-            await _unitOfWork.MenuItem.AddAsync(menuItem);
-            await _unitOfWork.SaveChangesAsync();
-            return Result.Success(200);
-        }
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<Models.IResult> CreateItemAsync(MenuItem menuItem)
+    {
+        await _unitOfWork.MenuItem.AddAsync(menuItem);
+        await _unitOfWork.SaveChangesAsync();
+        return Result.Success(200);
+    }
 
-        public async Task<Models.IResult> DeleteItemAsync(Guid Id)
+    public async Task<Models.IResult> DeleteItemAsync(Guid Id)
+    {
+        var MenuItem = await _unitOfWork.MenuCategory.GetByIdAsync(Id);
+        if(MenuItem is null) return Result.Failure(
+            StatusCodes.Status404NotFound,
+            Error.NotFound("Item not found!"));
+        await _unitOfWork.MenuItem.DeleteAsync(Id);
+        await _unitOfWork.SaveChangesAsync();
+        return Result.Success();
+    }
+
+    public async Task<IResult<MenuItemDto>> GetMenuItemByIdAsync(Guid Id)
+    {
+        var MenuItem = await _unitOfWork.MenuItem.GetByIdAsync(Id);
+        if (MenuItem == null)
         {
-            var MenuItem = await _unitOfWork.MenuCategory.GetByIdAsync(Id);
-            if(MenuItem is null) return Result.Failure(
+            return Result<MenuItemDto>.Failure(
                 StatusCodes.Status404NotFound,
-                Error.NotFound("Item not found!"));
-            await _unitOfWork.MenuItem.DeleteAsync(Id);
-            await _unitOfWork.SaveChangesAsync();
-            return Result.Success();
+                Error.NotFound("Item not found"));
         }
+        var menuItemDto = MenuItem.Adapt<MenuItemDto>();
+        return Result<MenuItemDto>.Success(menuItemDto);
     }
 }
