0c351be6465432afe45d59ceebec6d0f3ffbabd4
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-09 14:22:54 +0600

    feat: add GetAllMenuCategoriesAsync to interface

--- CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
+++ CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
@@ -7,5 +7,6 @@ public interface IMenuCategoryService
 {
     Task<IResult<Guid>> CreateCategoryAsync(MenuCategory menuCategory);
     Task<IResult<MenuCategoryDto>> GetMenuCategoryByIdAsync(Guid Id);
+    Task<IResult<List<MenuCategoryDto>>> GetAllMenuCategoriesAsync();
     Task<IResult> DeleteCategoryAsync(Guid Id);
 }
