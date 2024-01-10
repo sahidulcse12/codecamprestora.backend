518a692c2bffdccf8d0b6ff79bad5780fecd84cb
Md. Shafayet Maruf <professional.shafayetmaruf@gmail.com>
2024-01-10 11:59:02 +0600

    feat: created adapters for [UserManager], [RoleManager]

---
+++ CodeCampRestora.Infrastructure/Identity/Adapters/ApplicatonUserManagerAdapter.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Identity;
+using CodeCampRestora.Domain.Identity;
+using CodeCampRestora.Application.Attributes;
+using CodeCampRestora.Infrastructure.Identity.Interfaces;
+
+namespace CodeCampRestora.Infrastructure.Identity.Adapters;
+
+[ScopedLifetime]
+public class ApplicationUserManagerAdapter : IApplicationUserManagerAdapter
+{
+    private readonly IApplicationUserManager _applicationUserManager;
+
+    public ApplicationUserManagerAdapter(IApplicationUserManager applicatonUserManager)
+    {
+        _applicationUserManager = applicatonUserManager;
+    }
+
+    public async Task<ApplicationUser?> FindByEmailAsync(string email)
+    {
+        return await _applicationUserManager.FindByEmailAsync(email);
+    }
+
+    public async Task<ApplicationUser?> FindByIdAsync(string email)
+    {
+        return await _applicationUserManager.FindByIdAsync(email);
+    }
+
+    public async Task<ApplicationUser?> FindByNameAsync(string userName)
+    {
+        return await _applicationUserManager.FindByNameAsync(userName);
+    }
+
+    public async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+    {
+        return await _applicationUserManager.CheckPasswordAsync(user, password);
+    }
+
+    public async Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+    {
+        return await _applicationUserManager.CreateAsync(user, password);
+    }
+
+    public async Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role)
+    {
+        return await _applicationUserManager.AddToRoleAsync(user, role);
+    }
+
+    public async Task<IList<string>> GetRolesAsync(ApplicationUser user)
+    {
+        return await _applicationUserManager.GetRolesAsync(user);
+    }
+}
---
+++ CodeCampRestora.Infrastructure/Identity/Interfaces/IApplicationUserManager.cs
@@ -0,0 +1,15 @@
+using CodeCampRestora.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace CodeCampRestora.Infrastructure.Identity.Interfaces;
+
+public interface IApplicationUserManager
+{
+    Task<ApplicationUser?> FindByEmailAsync(string email);
+    Task<ApplicationUser?> FindByIdAsync(string id);
+    Task<ApplicationUser?> FindByNameAsync(string userName);
+    Task<bool> CheckPasswordAsync(ApplicationUser user, string password);
+    Task<IdentityResult> CreateAsync(ApplicationUser user, string password);
+    Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role);
+    Task<IList<string>> GetRolesAsync(ApplicationUser user);
+}
