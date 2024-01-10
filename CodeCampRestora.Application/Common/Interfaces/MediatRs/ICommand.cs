95f4bff89f0d7f7012a57624a0b15f441d5ab45f
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-10 13:42:59 +0600

    changes imagesHanlder

--- CodeCampRestora.Application/Common/Interfaces/MediatRs/ICommand.cs
+++ CodeCampRestora.Application/Common/Interfaces/MediatRs/ICommand.cs
@@ -24,5 +24,4 @@ public interface ICommandHandler<TRequest>
 public interface ICommandHandler<TRequest, TResponse>
     : IRequestHandler<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
-    Task<IResult<ReviewDTO>> Handle(CreateReviewCommand request, CancellationToken cancellationToken);
 }
