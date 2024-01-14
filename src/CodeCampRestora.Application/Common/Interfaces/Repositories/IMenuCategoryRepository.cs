af944f9eff0344de5f4dafe2a1ea53dba3bd3566
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-14 14:41:16 +0600

    feat: add GetAllByIdAsync to MenuCategoryRepository interface

--- src/CodeCampRestora.Application/Common/Interfaces/Repositories/IMenuCategoryRepository.cs
+++ src/CodeCampRestora.Application/Common/Interfaces/Repositories/IMenuCategoryRepository.cs
@@ -1,9 +1,11 @@
+using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace CodeCampRestora.Application.Common.Interfaces.Repositories
 {
     public interface IMenuCategoryRepository : IRepository<MenuCategory, Guid>
     {
-        
+        Task<List<MenuCategory>> GetAllByIdAsync(Guid Id);
     }
 }
