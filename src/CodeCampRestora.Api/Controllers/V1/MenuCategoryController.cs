5d033ab31442854f444567e9392ba70cc8f3bf1d
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-14 13:24:38 +0600

    feat: add GetAllMenuCategories controller

--- src/CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
+++ src/CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
@@ -6,6 +6,7 @@ using IResult =  CodeCampRestora.Application.Models.IResult;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using CodeCampRestora.Application.Models;
+using CodeCampRestora.Application.Features.MenuCategories.Commands.GetAllMenuCategory;
 
 namespace CodeCampRestora.Api.Controllers.V1;
 
@@ -30,6 +31,20 @@ public class MenuCategoryController : ApiBaseController
         Guid id)
     {
         var result = await Sender.Send(new GetMenuCategoryByIdQuery(id));
+        return result;
+    }
+
+    [HttpGet("GetAll{id:Guid}")]
+    [SwaggerOperation(
+        Summary = "Get all menu Categories",
+        Description = @"Sample Request:
+        Get: api/v1/MenuCategory/3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c"
+    )]
+    public async Task<IResult<List<MenuCategoryDto>>> GetAll(
+        [FromRoute, SwaggerParameter(Description = "Get all menu categories by restaurant id", Required = true)]
+        Guid id)
+    {
+        var result = await Sender.Send(new GetAllMenuCategoryQuery(id));
         return result;
     }
 
