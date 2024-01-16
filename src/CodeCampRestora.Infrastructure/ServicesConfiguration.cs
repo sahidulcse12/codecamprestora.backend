af2fce2ecaa8e4bdc624218838d85720324e64a1
rafi <irafiulislam50@gmail.com>
2024-01-16 14:17:50 +0600

    fix: switched production database from test database

--- src/CodeCampRestora.Infrastructure/ServicesConfiguration.cs
+++ src/CodeCampRestora.Infrastructure/ServicesConfiguration.cs
@@ -11,7 +11,7 @@ public static class ServicesConfiguration
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionStringKey = "SupaBaseConnection";
+        var connectionStringKey = "ProductionDbContext";
         var assemblyName = Assembly.GetExecutingAssembly().FullName;
         services.AddDbContext<ApplicationDbContext>(options =>
         {
