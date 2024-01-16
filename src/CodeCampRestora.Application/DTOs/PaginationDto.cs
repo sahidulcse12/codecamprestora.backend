db905ddb3e7814fec1be13e5d3a695515aaf6911
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-16 23:16:04 +0600

    fix: modify Data property type

--- src/CodeCampRestora.Application/DTOs/PaginationDto.cs
+++ src/CodeCampRestora.Application/DTOs/PaginationDto.cs
@@ -4,10 +4,10 @@ namespace CodeCampRestora.Application.DTOs
 {
     public class PaginationDto<T> where T : class
     {
-        public PagedList<T> Data { get; set; }
+        public List<T> Data { get; set; }
         public int TotalCount { get; set; }
         public int TotalPages { get; set; }
-        public PaginationDto(PagedList<T> data, int totalCount, int totalPages)
+        public PaginationDto(List<T> data, int totalCount, int totalPages)
         {
             Data = data;
             TotalCount = totalCount;
