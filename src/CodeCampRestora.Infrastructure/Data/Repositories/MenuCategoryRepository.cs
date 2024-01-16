4075eae48151ffafb85008ffdc99a407bdb21fc9
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-16 13:02:25 +0600

    fix: inject ApplicationDbContext in MenuCategoryRepository

--- src/CodeCampRestora.Infrastructure/Data/Repositories/MenuCategoryRepository.cs
+++ src/CodeCampRestora.Infrastructure/Data/Repositories/MenuCategoryRepository.cs
@@ -1,4 +1,5 @@
 using CodeCampRestora.Application.Attributes;
+using CodeCampRestora.Application.Common.Interfaces.DbContexts;
 using CodeCampRestora.Application.Common.Interfaces.Repositories;
 using CodeCampRestora.Domain.Entities;
 using CodeCampRestora.Infrastructure.Data.DbContexts;
@@ -10,10 +11,10 @@ namespace CodeCampRestora.Infrastructure.Data.Repositories;
 public class MenuCategoryRepository : Repository<MenuCategory, Guid>, IMenuCategoryRepository
 {
     private readonly DbSet<MenuCategory> _menuCategory;
-    public MenuCategoryRepository(ApplicationDbContext applicationDbContext) 
-    : base(applicationDbContext)
-    { 
-        _menuCategory = applicationDbContext.Set<MenuCategory>();
+    public MenuCategoryRepository(IApplicationDbContext applicationDbContext, ApplicationDbContext context) 
+    : base((DbContext)applicationDbContext)
+    {
+        _menuCategory = context.Set<MenuCategory>();
     }
 
     public async Task<List<MenuCategory>> GetAllByIdAsync(Guid Id)
