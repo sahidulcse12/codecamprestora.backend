690f65d3bd9727143e1c7e07bf98c0fe45ad1e18
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-16 23:22:12 +0600

    feat: implements GetPaginatedByIdAsync

--- src/CodeCampRestora.Infrastructure/Data/Repositories/MenuItemRepository.cs
+++ src/CodeCampRestora.Infrastructure/Data/Repositories/MenuItemRepository.cs
@@ -4,6 +4,7 @@ using CodeCampRestora.Application.Attributes;
 using CodeCampRestora.Application.Common.Interfaces.DbContexts;
 using CodeCampRestora.Application.Common.Interfaces.Repositories;
 using CodeCampRestora.Infrastructure.Data.DbContexts;
+using CodeCampRestora.Application.Common.Helpers.Pagination;
 
 namespace CodeCampRestora.Infrastructure.Data.Repositories;
 
@@ -22,5 +23,14 @@ public class MenuItemRepository : Repository<MenuItem,Guid>, IMenuItemRepository
     {
         var Entities = _menuItem.Where(e => e.BranchId == Id).ToList();
         return Entities;
+    }
+
+    public async Task<PagedList<MenuItem>> GetPaginatedByIdAsync(
+        Guid Id, int pageNumber, int pageSize
+    )
+    {
+        var Entities = _menuItem.Where(e => e.BranchId == Id);
+        var PagedList = await PagedList<MenuItem>.ToPagedListAsync(Entities, pageNumber, pageSize);
+        return PagedList;
     }
 }
