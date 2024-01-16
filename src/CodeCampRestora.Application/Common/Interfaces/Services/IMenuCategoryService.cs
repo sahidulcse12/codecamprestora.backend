9965e0fbe009dd348c68384ea2f5ae57b26f9508
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-16 17:36:15 +0600

    feat: add GetPaginationAsync to interface

--- src/CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
+++ src/CodeCampRestora.Application/Common/Interfaces/Services/IMenuCategoryService.cs
@@ -1,6 +1,7 @@
 using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Application.Features.MenuItems.Commands.CreateMenuCategory;
 using CodeCampRestora.Application.Models;
+using CodeCampRestora.Domain.Entities;
 
 namespace CodeCampRestora.Application.Common.Interfaces.Services;
 public interface IMenuCategoryService
@@ -9,4 +10,5 @@ public interface IMenuCategoryService
     Task<IResult<MenuCategoryDto>> GetMenuCategoryByIdAsync(Guid Id);
     Task<IResult<List<MenuCategoryDto>>> GetAllMenuCategoryAsync(Guid Id);
     Task<IResult> DeleteCategoryAsync(Guid Id);
+    Task<IResult<PaginationDto<MenuCategory>>> GetPaginatedMenuCategoryAsync(int pageNumber, int pageSize);
 }
--- src/CodeCampRestora.Application/Common/Interfaces/Services/IMenuItemService.cs
+++ src/CodeCampRestora.Application/Common/Interfaces/Services/IMenuItemService.cs
@@ -1,6 +1,8 @@
+using CodeCampRestora.Application.Common.Helpers.Pagination;
 using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Application.Features.MenuItems.Commands.CreateMenuItem;
 using CodeCampRestora.Application.Models;
+using CodeCampRestora.Domain.Entities;
 
 namespace CodeCampRestora.Application.Common.Interfaces.Services
 {
@@ -10,5 +12,6 @@ namespace CodeCampRestora.Application.Common.Interfaces.Services
         Task<IResult<MenuItemDto>> GetMenuItemByIdAsync(Guid Id);
         Task<IResult> DeleteItemAsync(Guid Id);
         Task<IResult<List<MenuItemDto>>> GetAllMenuItemsAsync(Guid id);
+        Task<IResult<PaginationDto<MenuItem>>> GetPaginatedAsync(int pageNumber, int pageSize);
     }
 }
