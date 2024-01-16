618bd6074b415f88bd869dd824b27ef1cf39413f
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-16 13:52:04 +0600

    feat: [Entities] Review is now IsReviewHidden

--- src/CodeCampRestora.Application/DTOs/ReviewDTO.cs
+++ src/CodeCampRestora.Application/DTOs/ReviewDTO.cs
@@ -5,8 +5,8 @@
         public  string? Description { set; get; }
         public  double Rating { set; get; }
         public Guid OrderId { set; get; }
-        public Guid BranchId {  set; get; }
-        public bool IsHideReview { get; set; }
-        
-    }
+        public bool IsReviewHidden { get; set; }
+
+
+}
 }
--- src/CodeCampRestora.Domain/Entities/Review.cs
+++ src/CodeCampRestora.Domain/Entities/Review.cs
@@ -1,4 +1,5 @@
-﻿using CodeCampRestora.Domain.Entities.Common;
+﻿using CodeCampRestora.Domain.Entities.Branches;
+using CodeCampRestora.Domain.Entities.Common;
 
 namespace CodeCampRestora.Domain.Entities
 {
@@ -7,8 +8,9 @@ namespace CodeCampRestora.Domain.Entities
         public  string? Description { set; get; }
         public  double Rating { set; get; }
         public Guid OrderId { set; get; }
-        public Guid BranchId { set; get; }
-        public bool IsHideReview { get; set; }
+        public Guid BrandId { set; get; }
+        public Branch? BranchId { set; get; }
+        public bool IsReviewHidden { get; set; }
 
     }
 }
