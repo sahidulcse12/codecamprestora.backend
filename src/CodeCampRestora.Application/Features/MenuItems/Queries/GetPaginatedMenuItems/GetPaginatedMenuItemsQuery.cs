4cd3e9b8ab9ab048143e9da8a6926c315c611639
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-16 23:17:06 +0600

    feat: pass branch id to Pagination query

--- src/CodeCampRestora.Application/Features/MenuItems/Queries/GetPaginatedMenuItems/GetPaginatedMenuItemsQuery.cs
+++ src/CodeCampRestora.Application/Features/MenuItems/Queries/GetPaginatedMenuItems/GetPaginatedMenuItemsQuery.cs
@@ -1,17 +1,18 @@
-using CodeCampRestora.Application.Common.Helpers.Pagination;
 using CodeCampRestora.Application.Common.Interfaces.MediatRs;
 using CodeCampRestora.Application.DTOs;
 using CodeCampRestora.Application.Models;
-using CodeCampRestora.Domain.Entities;
+
 
 namespace CodeCampRestora.Application.Features.MenuItems.Queries.GetPaginatedMenuItems;
 
-public record GetPaginatedMenuItemsQuery : IQuery<IResult<PaginationDto<MenuItem>>>
+public record GetPaginatedMenuItemsQuery : IQuery<IResult<PaginationDto<MenuItemDto>>>
 {
+    public Guid BranchId { get; set; }
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
-    public GetPaginatedMenuItemsQuery(int pageNumber, int pageSize)
+    public GetPaginatedMenuItemsQuery(Guid branchId, int pageNumber, int pageSize)
     {
+        BranchId = branchId;
         PageNumber = pageNumber;
         PageSize = pageSize;
     }
