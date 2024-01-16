318539080176dbdf24b72976e9880d612d2b3b62
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-16 13:53:19 +0600

    feat: add GetAllByIdAsync to repository

--- src/CodeCampRestora.Infrastructure/Data/Repositories/MenuItemRepository.cs
+++ src/CodeCampRestora.Infrastructure/Data/Repositories/MenuItemRepository.cs
@@ -3,14 +3,24 @@ using CodeCampRestora.Domain.Entities;
 using CodeCampRestora.Application.Attributes;
 using CodeCampRestora.Application.Common.Interfaces.DbContexts;
 using CodeCampRestora.Application.Common.Interfaces.Repositories;
+using CodeCampRestora.Infrastructure.Data.DbContexts;
 
 namespace CodeCampRestora.Infrastructure.Data.Repositories;
 
 [ScopedLifetime]
 public class MenuItemRepository : Repository<MenuItem,Guid>, IMenuItemRepository
 {
-    public MenuItemRepository(IApplicationDbContext applicationDbContext)
+    private readonly DbSet<MenuItem> _menuItem;
+    public MenuItemRepository(IApplicationDbContext applicationDbContext,
+    ApplicationDbContext context)
         : base((DbContext) applicationDbContext)
     {
+        _menuItem = context.Set<MenuItem>();
+    }
+
+    public async Task<List<MenuItem>> GetAllByIdAsync(Guid Id)
+    {
+        var Entities = _menuItem.Where(e => e.BranchId == Id).ToList();
+        return Entities;
     }
 }
