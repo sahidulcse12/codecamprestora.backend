b4c1a9050efbf6a396d236d97f3fb30b2f1242b0
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-17 16:58:50 +0600

    feat: implements UpdateMenuItemsAsync repository

--- src/CodeCampRestora.Infrastructure/Data/Repositories/MenuItemRepository.cs
+++ src/CodeCampRestora.Infrastructure/Data/Repositories/MenuItemRepository.cs
@@ -5,6 +5,7 @@ using CodeCampRestora.Application.Common.Interfaces.DbContexts;
 using CodeCampRestora.Application.Common.Interfaces.Repositories;
 using CodeCampRestora.Infrastructure.Data.DbContexts;
 using CodeCampRestora.Application.Common.Helpers.Pagination;
+using CodeCampRestora.Application.Models;
 
 namespace CodeCampRestora.Infrastructure.Data.Repositories;
 
@@ -12,11 +13,13 @@ namespace CodeCampRestora.Infrastructure.Data.Repositories;
 public class MenuItemRepository : Repository<MenuItem,Guid>, IMenuItemRepository
 {
     private readonly DbSet<MenuItem> _menuItem;
+    private readonly ApplicationDbContext _context;
     public MenuItemRepository(IApplicationDbContext applicationDbContext,
     ApplicationDbContext context)
         : base((DbContext) applicationDbContext)
     {
         _menuItem = context.Set<MenuItem>();
+        _context = context;
     }
 
     public async Task<List<MenuItem>> GetAllByIdAsync(Guid Id)
@@ -32,5 +35,25 @@ public class MenuItemRepository : Repository<MenuItem,Guid>, IMenuItemRepository
         var Entities = _menuItem.Where(e => e.BranchId == Id);
         var PagedList = await PagedList<MenuItem>.ToPagedListAsync(Entities, pageNumber, pageSize);
         return PagedList;
+    }
+
+    public async Task<IResult> UpdateMenuItemsAsync(List<MenuItem> newMenuItems)
+    {
+
+        foreach (var newMenuItem in newMenuItems)
+        {
+            var existingEntity = await _menuItem.FindAsync(newMenuItem.Id);
+
+            if (existingEntity == null)
+            {
+                return Result.Failure(Error.NotFound($"Menu item with Id {newMenuItem.Id} not found!"));
+            }
+
+            existingEntity.DisplayOrder =  newMenuItem.DisplayOrder;
+            _menuItem.Entry(existingEntity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        return Result.Success();
     }
 }
