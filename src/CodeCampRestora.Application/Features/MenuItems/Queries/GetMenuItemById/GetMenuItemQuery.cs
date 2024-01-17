564336c7d560e686c8e2e3287c5aa7a52851869f
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-17 12:51:21 +0600

    refactor: rename GetMenuItemById query

--- src/CodeCampRestora.Application/Features/MenuItems/Queries/GetMenuItemById/GetMenuItemQuery.cs
+++
@@ -1,8 +0,0 @@
-using CodeCampRestora.Application.Common.Interfaces.MediatRs;
-using CodeCampRestora.Application.DTOs;
-using CodeCampRestora.Application.Models;
-
-namespace CodeCampRestora.Application.Features.MenuItems.Queries
-{
-    public record GetMenuItemByIdQuery(Guid Id) : IQuery<IResult<MenuItemDto>>;
-}
