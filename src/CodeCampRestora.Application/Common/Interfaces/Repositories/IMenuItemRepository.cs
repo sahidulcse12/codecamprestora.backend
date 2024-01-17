08fa5b28c8087e29af124e54ae7d748cc8a4f236
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-17 16:55:46 +0600

    feat: add UpdateMenuItemsAsync to interface

--- src/CodeCampRestora.Application/Common/Interfaces/Repositories/IMenuItemRepository.cs
+++ src/CodeCampRestora.Application/Common/Interfaces/Repositories/IMenuItemRepository.cs
@@ -1,5 +1,5 @@
 using CodeCampRestora.Application.Common.Helpers.Pagination;
-using CodeCampRestora.Application.DTOs;
+using CodeCampRestora.Application.Models;
 using CodeCampRestora.Domain.Entities;
 
 namespace CodeCampRestora.Application.Common.Interfaces.Repositories;
@@ -8,4 +8,5 @@ public interface IMenuItemRepository : IRepository<MenuItem, Guid>
 {
     Task<List<MenuItem>> GetAllByIdAsync(Guid Id);
     Task<PagedList<MenuItem>> GetPaginatedByIdAsync(Guid Id, int pageNumber, int pageSize);
+    Task<IResult> UpdateMenuItemsAsync(List<MenuItem> newMenuItems);
 }
