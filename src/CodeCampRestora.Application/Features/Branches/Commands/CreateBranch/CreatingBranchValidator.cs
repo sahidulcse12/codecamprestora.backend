13c8037e1a584d7285b85f597bb50f3f64db531b
rafi <irafiulislam50@gmail.com>
2024-01-25 20:18:20 +0600

    fix: added lat log validation

--- src/CodeCampRestora.Application/Features/Branches/Commands/CreateBranch/CreatingBranchValidator.cs
+++ src/CodeCampRestora.Application/Features/Branches/Commands/CreateBranch/CreatingBranchValidator.cs
@@ -8,6 +8,12 @@ public class CreatingBranchValidator : ApplicationValidator<CreateBranchCommand>
     public CreatingBranchValidator()
     {
         RuleFor(item => item.Name).NotEmpty().WithMessage("Branch Name can't be empty");
-        RuleFor(item => item.Address).NotEmpty().WithMessage("Branch Adreess can't be empty");
+        RuleFor(model => model.Address.Latitude).LessThanOrEqualTo(90)
+                                .GreaterThanOrEqualTo(-90)
+                                .WithMessage("Latitude must be between -90 <-> 90 degress.");
+
+        RuleFor(model => model.Address.Longitude).LessThanOrEqualTo(180)
+                                         .GreaterThanOrEqualTo(-180)
+                                         .WithMessage("Longitude must be between -180 <-> 180 degress.");
     }
 }
