0a27f714db75c47871cd46fde4fa5287a2af1e26
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-28 15:45:21 +0600

    docs: add swagger response in controllers

--- src/CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
+++ src/CodeCampRestora.Api/Controllers/V1/MenuCategoryController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using CodeCampRestora.Domain.Entities;
 using CodeCampRestora.Application.DTOs;
 using Swashbuckle.AspNetCore.Annotations;
 using CodeCampRestora.Application.Models;
@@ -17,6 +16,7 @@ public class MenuCategoryController : ApiBaseController
 {
     [HttpPost]
     [SwaggerOperation(summary: "Create a menu category")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
     public async Task<IResult> Post([FromBody] CreateMenuCategoryCommand command)
     {
         var result = await Sender.Send(command);
@@ -29,6 +29,8 @@ public class MenuCategoryController : ApiBaseController
         Description = @"Sample Request:
         Get: api/v1/MenuCategory/3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c"
     )]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Menu category not found", typeof(IResult))]
     public async Task<IResult<MenuCategoryDto>> Get(
         [FromRoute, SwaggerParameter(Description = "Get menu category by id", Required = true)]
         Guid id)
@@ -43,6 +45,7 @@ public class MenuCategoryController : ApiBaseController
         Description = @"Sample Request:
         Get: api/v1/MenuCategory/3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c"
     )]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
     public async Task<IResult<List<MenuCategoryDto>>> GetAll(
         [FromRoute, SwaggerParameter(Description = "Get all menu categories by restaurant id", Required = true)]
         Guid id)
@@ -57,6 +60,8 @@ public class MenuCategoryController : ApiBaseController
         Description = @"Sample Request:
         Get: api/v1/MenuCategory/3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c"
     )]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
+    [SwaggerResponse(StatusCodes.Status404NotFound, "Menu item not found", typeof(IResult))]
     public async Task<IResult> Delete(
         [FromRoute, SwaggerParameter(Description = "Delete by id", Required = true)]
         Guid id)
@@ -67,6 +72,7 @@ public class MenuCategoryController : ApiBaseController
 
     [HttpPut]
     [SwaggerOperation(summary: "Update a menu category")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
     public async Task<IResult> Update([FromBody] UpdateMenuCategoryCommand command)
     {
         var result = await Sender.Send(command);
@@ -79,6 +85,7 @@ public class MenuCategoryController : ApiBaseController
         Description = @"Sample Request:
         Get: api/v1/MenuCategory/Paginated?RestaurantId=3d8cd15b-6414-4bbc-92f7-5d6e9d3e5c9c&PageNumber=1&PageSize=10"
     )]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
     public async Task<IResult<PaginationDto<MenuCategoryDto>>> GetPaginated(Guid RestaurantId, int PageNumber, int PageSize)
     {
         var result = await Sender.Send(new GetPaginatedMenuCategoriesQuery(RestaurantId, PageNumber, PageSize));
@@ -91,6 +98,7 @@ public class MenuCategoryController : ApiBaseController
         Description = @"Sample Request:
         Get: api/v1/MenuCategory/UpdateDisplayOrder"
     )]
+    [SwaggerResponse(StatusCodes.Status200OK, "Request Success", typeof(IResult))]
     public async Task<IResult> Update(List<MenuCategoryDto> menuCategories)
     {
         var result = await Sender.Send(new UpdateMenuCategoryDisplayOrderCommand(menuCategories));
