8e1bb5f37e00f6ad358b27ffb2ba3d401f939e43
sabbir0152 <sabbirhossain5458@gmail.com>
2024-01-29 21:47:52 +0600

    internal to pubilc

--- src/CodeCampRestora.Application/Features/Reviews/Queries/GetReviewById/GetReviewByIdQueryHandler.cs
+++ src/CodeCampRestora.Application/Features/Reviews/Queries/GetReviewById/GetReviewByIdQueryHandler.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeCampRestora.Application.Features.Reviews.Queries.GetReviewById
+﻿namespace CodeCampRestora.Application.Features.Reviews.Queries.GetReviewById
 {
-    internal class GetReviewByIdQueryHandler
+    public class GetReviewByIdQueryHandler
     {
+
     }
 }
