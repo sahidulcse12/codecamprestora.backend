778e3918b9fba52801c5a19939729fd5b8d132ba
Maheen Chowdhury <maheen.mn.c@gmail.com>
2024-01-31 15:04:20 +0600

    feat: add base64 property validator

--- src/CodeCampRestora.Application/Features/MenuCategories/Commands/CreateMenuCategory/CreateMenuCategoryCommandValidator.cs
+++ src/CodeCampRestora.Application/Features/MenuCategories/Commands/CreateMenuCategory/CreateMenuCategoryCommandValidator.cs
@@ -11,7 +11,22 @@ namespace CodeCampRestora.Application.Features.MenuCategories.Commands.CreateMen
         {
             RuleFor(item => item.Name).NotEmpty().WithMessage("Name is required.");
             RuleFor(item => item.Image).NotNull().WithMessage("Image can not be null!");
+            RuleFor(item => item.Image.Base64Url)
+                .Must(BeValidBase64).WithMessage("Image Base64Url is not a valid base64 string!");
             RuleFor(item => item.RestaurantId).NotNull().WithMessage("RestaurantId can not be null!");
+        }
+
+        private bool BeValidBase64(string base64String)
+        {
+            try
+            {
+                Convert.FromBase64String(base64String);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
         }
     }
 }
