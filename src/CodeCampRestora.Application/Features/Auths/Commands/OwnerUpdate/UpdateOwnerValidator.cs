3f26fb5271b0a06e1a45406641c2d958fb9e03bf
muhit <sahid.299350@gmail.com>
2024-02-01 09:28:46 +0600

    feat: update user information validator is implemented

--- src/CodeCampRestora.Application/Features/Auths/Commands/OwnerUpdate/UpdateOwnerValidator.cs
+++ src/CodeCampRestora.Application/Features/Auths/Commands/OwnerUpdate/UpdateOwnerValidator.cs
@@ -1,5 +1,13 @@
-﻿
+﻿using CodeCampRestora.Application.Common;
+using CodeCampRestora.Application.Extensions;
+using FluentValidation;
+
 namespace CodeCampRestora.Application.Features.Auths.Commands.OwnerUpdate;
-public class UpdateOwnerValidator
+public class UpdateOwnerValidator : ApplicationValidator<UpdateOwnerCommand>
 {
+    public UpdateOwnerValidator()
+    {
+        RuleFor(p => p.FullName).NotEmpty().WithMessage("FullName is required");
+        RuleFor(p=>p.NewPassword).Cascade(CascadeMode.Stop).ValidatePassword();
+    }
 }
